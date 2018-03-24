a3aa496ec639ae4bfa3485281b066a17fd13b3b1
Alex Gyujin Kim <agk2144@columbia.edu>
2018-03-24 18:35:29 -0400

    terminal startup sync

---
+++ .emacs.d/elpa/backup-walker-20130720.816/backup-walker-autoloads.el
@@ -0,0 +1,24 @@
+;;; backup-walker-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (directory-file-name (or (file-name-directory #$) (car load-path))))
+
+;;;### (autoloads nil "backup-walker" "backup-walker.el" (23222 53622
+;;;;;;  941983 124000))
+;;; Generated autoloads from backup-walker.el
+
+(autoload 'backup-walker-start "backup-walker" "\
+start walking with the latest backup
+
+with universal arg, ask for a file-name.
+
+\(fn ORIGINAL-FILE)" t nil)
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; backup-walker-autoloads.el ends here
